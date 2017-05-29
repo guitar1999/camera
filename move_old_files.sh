d2f406db442c447c4b464e718bfe5747e71d7269
Jesse Bishop <jessebishop@yahoo.com>
2017-05-29 10:29:39 -0400

    adding year

--- move_old_files.sh
+++ move_old_files.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-month=$1
-start=$2
-end=$3
+year=$1
+month=$2
+start=$3
+end=$4
 
 for i in $(seq -f %02g ${start} ${end})
 do 
-    aws s3 mv s3://lunt-security/2017${month}${i}_daily.mp4 s3://lunt-security/2017/${month}/2017${month}${i}_daily.mp4 --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+    aws s3 mv s3://lunt-security/${year}${month}${i}_daily.mp4 s3://lunt-security/${year}/${month}/${year}${month}${i}_daily.mp4 --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 done
