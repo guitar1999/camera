09bd93036262c99f71bf9d6f4fa0dcb03f57713c
Jesse Bishop <jessebishop@yahoo.com>
2019-01-05 12:06:04 -0500

    move to local storage

--- take_picture.sh
+++ take_picture.sh
@@ -5,3 +5,6 @@ sudo service motion stop
 /usr/bin/raspistill -hf -vf -o /var/local/camera/${now}.jpg
 sudo service motion start
 /usr/local/bin/aws s3 cp /var/local/camera/${now}.jpg s3://lunt-security/daily_images/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+sudo mount -t nfs ${NASIP}:/Storage /mnt/storage
+cp /var/local/camera/${now}.jpg /mnt/storage/Daily_Images/
+sudo umount /mnt/storage
